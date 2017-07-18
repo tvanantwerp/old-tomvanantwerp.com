07b8da020f46a835ae163308620000200a6f6793
tvanantwerp <tom@tomvanantwerp.com>
2017-07-18 14:12:41 -0400

    Continued cleanup

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
     var links = document.getElementsByTagName('a');
     for (var i = 0, j = links.length; i < j; i++) {
       var host = links[i].host;
-      if (host.indexOf('tomvanantwerp.com') === -1 && host.indexOf('localhost') === -1) {
+      if (host.indexOf('tomvanantwerp.com') === -1) {
         links[i].target = '_blank';
       }
     }
